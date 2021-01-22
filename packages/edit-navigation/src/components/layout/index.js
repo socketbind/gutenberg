0ccc8c4a57e6b05495b3b092f5ab6aeeb4c9ba08
Daniel Richards <daniel.richards@automattic.com>
2021-01-22 17:54:41 +0800

    Add loading spinner to empty state

--- packages/edit-navigation/src/components/layout/index.js
+++ packages/edit-navigation/src/components/layout/index.js
@@ -65,7 +65,7 @@ export default function Layout( { blockEditorSettings } ) {
 						/>
 
 						{ ! navigationPost && (
-							<EditorEmptyState isPending={ hasLoadedMenus } />
+							<EditorEmptyState isPending={ ! hasLoadedMenus } />
 						) }
 
 						{ navigationPost && (
